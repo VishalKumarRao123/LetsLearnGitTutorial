6672f0fc5ec7208ff15cc3e1b67cabda09d5dbc2
Vishal Kumar Rao <vishalbth99@gmail.com>
2021-10-04 00:07:30 +0530

    Added modification in sum.java

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 class sum{
  public static void func() {
-  System.out.println("I am in Sum function");
+  System.out.println("Sum of 5 and 6 is : "+(5+6));
  }
  public static void main(String[] args) {
   func();
